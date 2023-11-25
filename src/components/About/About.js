22f457dcd5dfa4c45358606b131acd1207920de7
Vladimir Frolov <frolov_vo@inbox.ru>
2023-11-25 19:35:44 +0300

    feat: add about section

---
+++ src/components/About/About.js
@@ -0,0 +1,16 @@
+import './About.scss';
+
+function About() {
+  return (
+    <section className="about">
+      <h2 className="about__title">Чемпионы мира по флористике</h2>
+      <ul className="about__list">
+        <li className="about__list-item">современные линейки букетов</li>
+        <li className="about__list-item">сделаем в течение часа</li>
+        <li className="about__list-item">доставим быстро и бесплатно</li>
+      </ul>
+    </section>
+  );
+}
+
+export default About;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,15 @@
 import './App.scss';
 import Header from '../Header/Header';
+import Main from '../Main/Main';
+import About from '../About/About';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main>
+        <About />
+      </Main>
     </div>
   );
 }
