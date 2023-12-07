76953ed434fe7d530e7ddf94a5de677fb621913e
Vladimir Frolov <frolov_vo@inbox.ru>
2023-12-07 21:55:39 +0300

    feat: add instagram section

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import Header from '../Header/Header';
 import Main from '../Main/Main';
 import About from '../About/About';
 import Minics from '../Minics/Minics';
+import Instagram from '../Instagram/Instagram';
 import videosData from '../../utils/videosData';
 import { socialsData } from '../../utils/constants';
 
@@ -13,6 +14,7 @@ function App() {
       <Main>
         <About />
         <Minics videosData={videosData} socialsData={socialsData} />
+        <Instagram />
       </Main>
     </div>
   );
---
+++ src/components/Instagram/Instagram.js
@@ -0,0 +1,20 @@
+import './Instagram.scss';
+
+function Instagram() {
+  return (
+    <section className="instagram">
+      <h2 className="instagram__title">
+        Сотни современных букетов от 1 500 до 50 000 ₽
+      </h2>
+      <ul className="instagram__list">
+        <li className="instagram__list-item">в нашем Instagram </li>
+        <li className="instagram__list-item">пришлем фото перед отправкой</li>
+        <li className="instagram__list-item">
+          подпишем открытку каллиграфическим почерком
+        </li>
+      </ul>
+    </section>
+  );
+}
+
+export default Instagram;
