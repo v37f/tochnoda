8cada8c84ca4d345873b02ac13a5b66deb34426d
Vladimir Frolov <frolov_vo@inbox.ru>
2023-12-18 17:23:50 +0300

    fix: add keys to clients map

--- src/components/Clients/Clients.js
+++ src/components/Clients/Clients.js
@@ -5,12 +5,8 @@ function Clients({ clientsData }) {
     <section className="clients">
       <h2 className="clients__title">Наши клиенты</h2>
       <ul className="clients__list">
-        {clientsData.map((client) => (
-          <img
-            className="clients__list-item"
-            src={client.logo}
-            alt={client.title}
-          />
+        {clientsData.map(({ id, logo, title }) => (
+          <img className="clients__list-item" src={logo} alt={title} key={id} />
         ))}
       </ul>
     </section>
