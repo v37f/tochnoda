b343b127f974c6396e8938502d9861c780589328
Vladimir Frolov <frolov_vo@inbox.ru>
2023-12-30 02:23:59 +0300

    fix: change about title tag

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,7 @@ import './About.scss';
 function About() {
   return (
     <section className="about" id="about">
-      <h2 className="about__title">Чемпионы мира по флористике</h2>
+      <h1 className="about__title">Чемпионы мира по флористике</h1>
       <ul className="about__list">
         <li className="about__list-item">современные линейки букетов</li>
         <li className="about__list-item">сделаем в течение часа</li>
